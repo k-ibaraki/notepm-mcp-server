6bed93835dcc3b72649a98b6e518a64c059d8ad1
k-ibaraki <ibaraki@ncdc.co.jp>
2025-04-08 21:02:20 +0900

    chore: update descriptions

--- notepm_mcp_server/notepm.py
+++ notepm_mcp_server/notepm.py
@@ -7,6 +7,7 @@ import httpx
 import os
 from typing import Optional
 from dotenv import load_dotenv
+import json
 
 # 環境変数の読み込み
 load_dotenv()
@@ -156,7 +157,9 @@ async def serve() -> None:
             Tool(
                 name="search_notepm",
                 description="""
-                    NotePMで指定されたクエリを検索します。
+                    NotePM(ノートPM)で指定されたクエリを検索します。
+                    検索ワードは単語のAND検索です。自然言語での検索はサポートされていません。
+                    検索結果はJSON形式で返されます。
                     記事の本文が長い場合は、本文の全文が返されないことがあります。
                     全文を取得するには、get_notepm_page_detailを使用してください。
                 """,
@@ -164,7 +167,7 @@ async def serve() -> None:
             ),
             Tool(
                 name="get_notepm_page_detail",
-                description="NotePMで指定されたページコードの詳細を取得します。",
+                description="NotePM(ノートPM)で指定されたページコードの記事に対して詳細な内容を取得します。",
                 inputSchema=NotePMDetailParams.schema(),
             )
         ]
